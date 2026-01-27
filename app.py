e355a4288e856bfc7ddb7a53e0bfb419202c8b01
jilicuerda <132850859+jilicuerda@users.noreply.github.com>
2026-01-27 10:45:56 +0100

    Refactor app.py to include render_template

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import time
 import requests
 import urllib.parse # Added this to handle spaces in names (e.g. Magic Tostada)
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -93,4 +93,5 @@ def get_history():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
+
     app.run(host='0.0.0.0', port=port)
