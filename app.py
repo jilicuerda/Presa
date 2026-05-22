90f59c821f26ff2c1fb5e126720f2ad84261fe40
jilicuerda <132850859+jilicuerda@users.noreply.github.com>
2026-05-22 14:44:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -407,6 +407,10 @@ def ingest_match():
         return jsonify({"success": True})
     except: return jsonify({"error": "Error processing match or already in DB."}), 400
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
